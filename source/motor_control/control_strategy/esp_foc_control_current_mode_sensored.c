5a2c6ce15169d07b94ec6618e6f578cc6863b0b3
Felipe Neves <ryukokki.felipe@gmail.com>
2025-11-13 14:47:10 -0300

    motor_control: refactor again the motor control core and
    use only the slow mode as base strategy, also separate the
    core control components into motor control and observers.
    
    Signed-off-by: Felipe Neves <ryukokki.felipe@gmail.com>

--- source/motor_control/control_strategy/esp_foc_control_current_mode_sensored.c
+++ source/motor_control/control_strategy/esp_foc_control_current_mode_sensored.c
@@ -37,62 +37,10 @@
 #define ESP_FOC_DEBUG_PIN                  22
 static const char * tag = "ESP_FOC_CONTROL";
 
+
 IRAM_ATTR static void inverter_isr(void *data)
 {
     esp_foc_axis_t *axis = (esp_foc_axis_t *)data;
-    axis->isensor_driver->sample_isensors(axis->isensor_driver);
-}
-
-IRAM_ATTR void do_current_mode_sensored_high_speed_loop(void *arg)
-{
-    esp_foc_axis_t *axis = (esp_foc_axis_t *)arg;
-
-#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
-    gpio_set_level(ESP_FOC_DEBUG_PIN, true);
-#endif
-
-    isensor_values_t val;
-    axis->isensor_driver->fetch_isensors(axis->isensor_driver, &val);
-    axis->i_u = val.iu_axis_0;
-    axis->i_v = val.iv_axis_0;
-    axis->i_w = val.iw_axis_0;
-
-    float ext_delta = (axis->dt * (ESP_FOC_LOW_SPEED_DOWNSAMPLING - axis->downsampling_low_speed));
-    axis->extrapolated_rotor_position = axis->rotor_position + axis->current_speed * ext_delta;
-    axis->rotor_elec_angle = esp_foc_mechanical_to_elec_angle(axis->extrapolated_rotor_position, axis->motor_pole_pairs);
-    axis->rotor_elec_angle = esp_foc_normalize_angle(axis->rotor_elec_angle);
-    float e_sin = esp_foc_sine(axis->rotor_elec_angle);
-    float e_cos = esp_foc_cosine(axis->rotor_elec_angle);
-
-    esp_foc_get_dq_currents(e_sin,
-        e_cos,
-        axis->i_u,
-        axis->i_v,
-        axis->i_w,
-        &axis->i_alpha.raw,
-        &axis->i_beta.raw,
-        &axis->i_q.raw,
-        &axis->i_d.raw);
-
-    esp_foc_current_control_loop(axis);
-
-    esp_foc_modulate_dq_voltage(e_sin,
-                    e_cos,
-                    axis->u_d.raw,
-                    axis->u_q.raw,
-                    &axis->u_alpha.raw,
-                    &axis->u_beta.raw,
-                    &axis->u_u.raw,
-                    &axis->u_v.raw,
-                    &axis->u_w.raw,
-                    axis->biased_dc_link_voltage,
-                    axis->dc_link_to_normalized);
-
-
-    axis->inverter_driver->set_voltages(axis->inverter_driver,
-                                        axis->u_u.raw,
-                                        axis->u_v.raw,
-                                        axis->u_w.raw);
 
     if(axis->downsampling_low_speed) {
         axis->downsampling_low_speed--;
@@ -101,18 +49,24 @@ IRAM_ATTR void do_current_mode_sensored_high_speed_loop(void *arg)
             esp_foc_send_notification(axis->low_speed_ev);
         }
     }
+}
 
-#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
-    gpio_set_level(ESP_FOC_DEBUG_PIN, false);
-#endif
-
+IRAM_ATTR void do_current_mode_sensored_high_speed_loop(void *arg)
+{
+    /* timestamp the current readings */
+    uint64_t * current_timestamp = (uint64_t *)arg;
+    *current_timestamp = esp_foc_now_useconds();
 }
 
 IRAM_ATTR void do_current_mode_sensored_low_speed_loop(void *arg)
 {
     esp_foc_axis_t *axis = (esp_foc_axis_t *)arg;
-    float low_speed_inv_dt = (axis->inv_dt / 10.0f);
+    isensor_values_t ival;
+    float low_speed_inv_dt = 1.0f / (axis->dt * ESP_FOC_LOW_SPEED_DOWNSAMPLING);
     float raw_speed;
+    float elec_delta;
+    float theta_timestamp = 0.0f;
+    uint64_t current_timestamp = 0;
 
 #ifdef CONFIG_ESP_FOC_SCOPE
     esp_foc_control_data_t control_data;
@@ -137,30 +91,82 @@ IRAM_ATTR void do_current_mode_sensored_low_speed_loop(void *arg)
 
     axis->isensor_driver->set_isensor_callback(axis->isensor_driver,
         axis->high_speed_loop_cb,
-        axis);
+        &current_timestamp);
 
     while(1) {
         esp_foc_wait_notifier();
 
-        axis->rotor_position = axis->rotor_sensor_driver->read_counts(axis->rotor_sensor_driver) *
-                            axis->shaft_ticks_to_radians_ratio * axis->natural_direction;
+#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
+        gpio_set_level(ESP_FOC_DEBUG_PIN, true);
+#endif
+        /* The ADC readings are already buffered while the Angle sensor is being read */
+        axis->isensor_driver->fetch_isensors(axis->isensor_driver, &ival);
+        axis->i_u = ival.iu_axis_0;
+        axis->i_v = ival.iv_axis_0;
+        axis->i_w = ival.iw_axis_0;
 
-        raw_speed = axis->rotor_position - axis->rotor_position_prev;
-        if (raw_speed >  M_PI)  raw_speed -= 2*M_PI;
-        if (raw_speed < -M_PI)  raw_speed += 2*M_PI;
-        raw_speed *= low_speed_inv_dt;
+        /* After picking the previous sample start the new one immediately (it will be used on the next loop step )*/
+        axis->isensor_driver->sample_isensors(axis->isensor_driver);
+
+        axis->rotor_position = axis->rotor_sensor_driver->read_counts(axis->rotor_sensor_driver) *
+                                axis->shaft_ticks_to_radians_ratio * axis->natural_direction;
+
+        theta_timestamp = fabs(esp_foc_now_seconds() - ((float)(current_timestamp) * 0.000001f));
+
+        elec_delta = esp_foc_mechanical_to_elec_angle(axis->current_speed, axis->motor_pole_pairs);
+        axis->rotor_elec_angle = esp_foc_mechanical_to_elec_angle(axis->rotor_position, axis->motor_pole_pairs);
+        axis->rotor_elec_angle = esp_foc_normalize_angle(axis->rotor_elec_angle - (elec_delta * theta_timestamp));
+
+
+        float e_sin = esp_foc_sine(axis->rotor_elec_angle);
+        float e_cos = esp_foc_cosine(axis->rotor_elec_angle);
+
+        raw_speed = (axis->rotor_position - axis->rotor_position_prev) * low_speed_inv_dt;
 
         esp_foc_critical_enter();
-        axis->current_speed = esp_foc_low_pass_filter_update( &axis->velocity_filter, raw_speed);
+        axis->current_speed = esp_foc_low_pass_filter_update(&axis->velocity_filter, raw_speed);
         axis->rotor_position_prev = axis->rotor_position;
         esp_foc_critical_leave();
 
         esp_foc_position_control_loop(axis);
         esp_foc_velocity_control_loop(axis);
 
+        esp_foc_get_dq_currents(e_sin,
+            e_cos,
+            axis->i_u,
+            axis->i_v,
+            axis->i_w,
+            &axis->i_alpha.raw,
+            &axis->i_beta.raw,
+            &axis->i_q.raw,
+            &axis->i_d.raw);
+
+        esp_foc_current_control_loop(axis);
+
+        esp_foc_modulate_dq_voltage(e_sin,
+                        e_cos,
+                        axis->u_d.raw,
+                        axis->u_q.raw,
+                        &axis->u_alpha.raw,
+                        &axis->u_beta.raw,
+                        &axis->u_u.raw,
+                        &axis->u_v.raw,
+                        &axis->u_w.raw,
+                        axis->biased_dc_link_voltage,
+                        axis->dc_link_to_normalized);
+
+        axis->inverter_driver->set_voltages(axis->inverter_driver,
+                                            axis->u_u.raw,
+                                            axis->u_v.raw,
+                                            axis->u_w.raw);
+
 #ifdef CONFIG_ESP_FOC_SCOPE
         esp_foc_get_control_data(axis, &control_data);
         esp_foc_scope_data_push(&control_data);
+#endif
+
+#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
+        gpio_set_level(ESP_FOC_DEBUG_PIN, false);
 #endif
     }
 }
--- source/motor_control/control_strategy/esp_foc_control_voltage_mode_sensored.c
+++ source/motor_control/control_strategy/esp_foc_control_voltage_mode_sensored.c
@@ -35,46 +35,11 @@
 #include "espFoC/esp_foc_pll_observer.h"
 
 #define ESP_FOC_DEBUG_PIN                  22
-
 static const char * tag = "ESP_FOC_CONTROL";
 
-
-/* Voltage mode core controllers */
-IRAM_ATTR void do_voltage_mode_sensored_high_speed_loop(void *arg)
+IRAM_ATTR static void inverter_isr(void *data)
 {
-    esp_foc_axis_t *axis = (esp_foc_axis_t *)arg;
-#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
-    gpio_set_level(ESP_FOC_DEBUG_PIN, true);
-#endif
-
-    float ext_delta = (axis->dt * (ESP_FOC_LOW_SPEED_DOWNSAMPLING - axis->downsampling_low_speed));
-    axis->extrapolated_rotor_position = axis->rotor_position + axis->current_speed * ext_delta;
-    axis->rotor_elec_angle = esp_foc_mechanical_to_elec_angle(axis->extrapolated_rotor_position, axis->motor_pole_pairs);
-    axis->rotor_elec_angle = esp_foc_normalize_angle(axis->rotor_elec_angle);
-    float e_sin = esp_foc_sine(axis->rotor_elec_angle);
-    float e_cos = esp_foc_cosine(axis->rotor_elec_angle);
-
-
-    axis->u_q.raw = axis->target_u_q.raw + axis->target_i_q.raw;
-    axis->u_d.raw = axis->target_u_d.raw + axis->target_i_d.raw;
-
-    esp_foc_modulate_dq_voltage(e_sin,
-                    e_cos,
-                    axis->u_d.raw,
-                    axis->u_q.raw,
-                    &axis->u_alpha.raw,
-                    &axis->u_beta.raw,
-                    &axis->u_u.raw,
-                    &axis->u_v.raw,
-                    &axis->u_w.raw,
-                    axis->biased_dc_link_voltage,
-                    axis->dc_link_to_normalized);
-
-
-    axis->inverter_driver->set_voltages(axis->inverter_driver,
-                                        axis->u_u.raw,
-                                        axis->u_v.raw,
-                                        axis->u_w.raw);
+    esp_foc_axis_t *axis = (esp_foc_axis_t *)data;
 
     if(axis->downsampling_low_speed) {
         axis->downsampling_low_speed--;
@@ -83,17 +48,18 @@ IRAM_ATTR void do_voltage_mode_sensored_high_speed_loop(void *arg)
             esp_foc_send_notification(axis->low_speed_ev);
         }
     }
+}
 
-#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
-    gpio_set_level(ESP_FOC_DEBUG_PIN, false);
-#endif
-
+IRAM_ATTR void do_voltage_mode_sensored_high_speed_loop(void *arg)
+{
+    /* Nothing to do in the ADC callback */
+    (void)arg;
 }
 
 IRAM_ATTR void do_voltage_mode_sensored_low_speed_loop(void *arg)
 {
     esp_foc_axis_t *axis = (esp_foc_axis_t *)arg;
-    float low_speed_inv_dt = (axis->inv_dt / ESP_FOC_LOW_SPEED_DOWNSAMPLING);
+    float low_speed_inv_dt = 1.0f / (axis->dt * ESP_FOC_LOW_SPEED_DOWNSAMPLING);
     float raw_speed;
 
 #ifdef CONFIG_ESP_FOC_SCOPE
@@ -103,7 +69,7 @@ IRAM_ATTR void do_voltage_mode_sensored_low_speed_loop(void *arg)
     axis->low_speed_ev = esp_foc_get_event_handle();
     axis->downsampling_low_speed = ESP_FOC_LOW_SPEED_DOWNSAMPLING;
 
-    ESP_LOGI(tag,"Starting the voltage mode low speed loop");
+    ESP_LOGI(tag,"Starting the voltage mode sensored low speed loop");
 
     axis->rotor_shaft_ticks = axis->rotor_sensor_driver->read_counts(axis->rotor_sensor_driver);
     axis->rotor_position =
@@ -111,35 +77,67 @@ IRAM_ATTR void do_voltage_mode_sensored_low_speed_loop(void *arg)
     axis->rotor_position_prev = axis->rotor_position;
     axis->current_speed = 0.0f;
 
-    ESP_LOGI(tag,"Starting voltage mode high speed loop");
+    ESP_LOGI(tag,"Starting voltage mode sensored high speed loop");
 
     axis->inverter_driver->set_inverter_callback(axis->inverter_driver,
-        axis->high_speed_loop_cb,
+        inverter_isr,
         axis);
 
     while(1) {
         esp_foc_wait_notifier();
 
-
+#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
+        gpio_set_level(ESP_FOC_DEBUG_PIN, true);
+#endif
         axis->rotor_position = axis->rotor_sensor_driver->read_counts(axis->rotor_sensor_driver) *
-                            axis->shaft_ticks_to_radians_ratio * axis->natural_direction;
+        axis->shaft_ticks_to_radians_ratio * axis->natural_direction;
+        axis->rotor_elec_angle = esp_foc_mechanical_to_elec_angle(axis->rotor_position, axis->motor_pole_pairs);
+        axis->rotor_elec_angle = esp_foc_normalize_angle(axis->rotor_elec_angle);
 
-        raw_speed = axis->rotor_position - axis->rotor_position_prev;
-        if (raw_speed >  M_PI)  raw_speed -= 2*M_PI;
-        if (raw_speed < -M_PI)  raw_speed += 2*M_PI;
-        raw_speed *= low_speed_inv_dt;
+        float e_sin = esp_foc_sine(axis->rotor_elec_angle);
+        float e_cos = esp_foc_cosine(axis->rotor_elec_angle);
+
+        raw_speed = (axis->rotor_position - axis->rotor_position_prev) * low_speed_inv_dt;
 
         esp_foc_critical_enter();
-        axis->current_speed = esp_foc_low_pass_filter_update( &axis->velocity_filter, raw_speed);
+        axis->current_speed = esp_foc_low_pass_filter_update(&axis->velocity_filter, raw_speed);
         axis->rotor_position_prev = axis->rotor_position;
         esp_foc_critical_leave();
 
         esp_foc_position_control_loop(axis);
         esp_foc_velocity_control_loop(axis);
 
-#ifdef CONFIG_ESP_FOC_SCOPE
+        axis->u_q.raw = axis->target_u_q.raw + axis->target_i_q.raw;
+        axis->u_d.raw = axis->target_u_d.raw + axis->target_i_d.raw;
+
+        esp_foc_modulate_dq_voltage(e_sin,
+                        e_cos,
+                        axis->u_d.raw,
+                        axis->u_q.raw,
+                        &axis->u_alpha.raw,
+                        &axis->u_beta.raw,
+                        &axis->u_u.raw,
+                        &axis->u_v.raw,
+                        &axis->u_w.raw,
+                        axis->biased_dc_link_voltage,
+                        axis->dc_link_to_normalized);
+
+        axis->inverter_driver->set_voltages(axis->inverter_driver,
+                                            axis->u_u.raw,
+                                            axis->u_v.raw,
+                                            axis->u_w.raw);
+
+
+        /* call the dummy high speed loop just to keep the compiler quiet */
+        do_slow_voltage_mode_sensored_high_speed_loop(axis);
+
+        #ifdef CONFIG_ESP_FOC_SCOPE
         esp_foc_get_control_data(axis, &control_data);
         esp_foc_scope_data_push(&control_data);
+#endif
+
+#ifdef CONFIG_ESP_FOC_DEBUG_CORE_TIMING
+        gpio_set_level(ESP_FOC_DEBUG_PIN, false);
 #endif
     }
 }
